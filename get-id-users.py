567a21d5566262196a0e8480ea850c059edb575b
pavTiger <pavTiger@gmail.com>
2019-11-23 15:59:43 +0300

    add input

--- get-id-users.py
+++ get-id-users.py
@@ -1,10 +1,10 @@
 # https://vk.com/dev/groups
 import vk_api
 
-def get_id_users():
+def get_id_users(group):
     idd = 'f58f9323564c7f94a25546fd2c30ba07619d2695cebcb65acb0e004154f0891de989b5a09a621db83a143'
     vk_session = vk_api.VkApi(token=idd)
-    return vk_session.method('groups.getMembers', {'group_id': 80799846})['items']
+    return vk_session.method('groups.getMembers', {'group_id': group})['items']
 
 if __name__ == '__main__':
-    print(get_id_users())
+    print(get_id_users(80799846))
