3a4b9e02bc97c5778487127780328201390ffbc7
lehatrutenb <lehatrutenb@gmail.com>
2019-11-23 16:57:13 +0300

    groups location 1.1

--- get_posts_data.py
+++ get_posts_data.py
@@ -1,16 +1,40 @@
 import vk_api
-#from get-id-users import get_id_users
+from get_id_users import get_id_users
 
 def get_posts_data(group_id):
-    #users_id = get_id_users(group_id)
+    users_id = get_id_users(group_id)
     vk_session = vk_api.VkApi(token='f58f9323564c7f94a25546fd2c30ba07619d2695cebcb65acb0e004154f0891de989b5a09a621db83a143')
     points = []
-    #for id in users_id:
-        #posts = vk_session.method('wall.get', {'owner_id': id, 'count': 100})
-    posts = vk_session.method('wall.get', {'owner_id': 226685733, 'count': 100})
-    for post in range(len(posts['items'])):
-        if 'geo' in posts['items'][post].keys():
-            geo = posts['items'][post]['geo']
-            points.append([geo['coordinates'].split()[0], geo['coordinates'].split()[1]])
-    print(points)
+    for id in users_id:
+        try:
+            posts = vk_session.method('wall.get', {'owner_id': id, 'count': 25})
+            # print('not_bad_error')
+        except:
+            # print('bad_error')
+            pass
+        z = 0
+        for post in range(len(posts['items'])):
+            if 'geo' in posts['items'][post].keys():
+                z += 1
+                geo = posts['items'][post]['geo']
+                points.append([geo['coordinates'].split()[0], geo['coordinates'].split()[1]])
+                # print(points)
+
+        if z != 0:
+            try:
+                posts = vk_session.method('wall.get', {'owner_id': id, 'count': 200, 'offset': 25})
+                # print('not_bad_error')
+            except:
+                # print('bad_error')
+                pass
+            z = 0
+            for post in range(len(posts['items'])):
+                if 'geo' in posts['items'][post].keys():
+                    z += 1
+                    geo = posts['items'][post]['geo']
+                    points.append([geo['coordinates'].split()[0], geo['coordinates'].split()[1]])
+                    # print(points)
+    return points
 get_posts_data(80799846)
+if __name__ == "main":
+    get_posts_data(80799846)
