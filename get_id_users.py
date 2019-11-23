b31e034cad7dfd310f21a0389d2e3ea510c46172
lehatrutenb <lehatrutenb@gmail.com>
2019-11-23 21:00:14 +0300

    add group name check

--- get_id_users.py
+++ get_id_users.py
@@ -5,7 +5,10 @@ def get_id_users(group):
     idd = 'f58f9323564c7f94a25546fd2c30ba07619d2695cebcb65acb0e004154f0891de989b5a09a621db83a143'
     vk_session = vk_api.VkApi(token=idd)
     #print(vk_session.method('groups.getMembers', {'group_id': group}))
-    return vk_session.method('groups.getMembers', {'group_id': group})['items']
+    try:
+        return vk_session.method('groups.getMembers', {'group_id': group})['items']
+    except:
+        return vk_session.method('groups.getMembers', {'group_id': 80799846})['items']
 
 if __name__ == '__main__':
     print(get_id_users(80799846))
