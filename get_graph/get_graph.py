0c564c7c5996d2a1607b621405cdb638cd5f6125
Dima <dimafedkk@mail.ru>
2019-11-23 17:44:31 +0300

    add get_graph.py

---
+++ get_graph/get_graph.py
@@ -0,0 +1,14 @@
+def get_graph(tag):
+    instagram = Instagram()
+    instagram.with_credentials('dimafedkk75', '030693Fsa')
+    instagram.login()
+    
+    medias = instagram.get_medias_by_tag(tag, count=1000)
+    a = []
+    for i in range(24):
+        a.append(0)
+    for posts in medias:
+        a[int(str(datetime.datetime.fromtimestamp(posts.created_time))[11:13])] += 1
+    fig = plt.figure()
+    graph1 = plt.plot([i for i in range(24)],a)
+    plt.show()
