c304a6335da5e03b8ef73b58dbde384c05b7c89f
lehatrutenb <lehatrutenb@gmail.com>
2019-11-23 21:26:01 +0300

    json converter

--- matrix_to_geojson.py
+++ matrix_to_geojson.py
@@ -1,8 +1,10 @@
-import geojson
+import json
 
 def arr_to_geojson(arr):
-    my_point = geojson.Point(arr)
-    dump = geojson.dumps(my_point, sort_keys=True)
+    d = {}
+    for i in range(len(arr)):
+        d[i] = {'latitude': arr[i][0], 'longitude': arr[i][1]}
+    dump = json.dumps(d)
     return dump
 
 if __name__ == '__main__':
